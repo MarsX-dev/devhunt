0a44b0d2433e48f5cd287b1828a56bd40cae1146
mac <mac@guests-MacBook-Pro.local>
2023-05-24 20:08:17 +0000

    Create user comments

---
+++ components/ui/Comment/Comment.tsx
@@ -0,0 +1,9 @@
+import mergeTW from "@/libs/mergeTW";
+import { ReactNode } from "react";
+
+export const Comment = ({ children, className }: { children: ReactNode, className?: string }) => (
+    <li className={mergeTW(`relative flex gap-6 ${className}`)}>
+        <span className="absolute left-[18px] top-10 bottom-0 my-auto h-[67%] w-1 bg-slate-800 comment-divided-bar"></span>
+        { children }
+    </li>
+)
--- components/ui/CommentForm/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./CommentForm"
-export * from "./CommentWrapper"
-export * from "./CommentUserAvatar"
-export * from "./CommentTextarea"
