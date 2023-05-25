ddfce335b0aec626ab20d9dfacfa522cae8bc93d
mac <mac@guests-MacBook-Pro.local>
2023-05-25 16:16:25 +0000

    Improve the tool page structure to make tabs work

--- app/tool/[slug]/page.tsx
+++ app/tool/[slug]/page.tsx
@@ -123,7 +123,7 @@ export default function Page() {
   ]
 
   return (
-    <section className="mt-20 pb-10 space-y-20">
+    <section className="mt-20 pb-10">
       <div className="container-custom-screen" id="about">
         <ProductLogo
           src="/images/wundergraph.png"
@@ -139,109 +139,112 @@ export default function Page() {
           <ButtonUpvote count={490} />
         </div>
       </div>
-      <div>
-        <Tabs className="sticky top-[3.75rem] z-30 bg-slate-900 md:top-[4.3rem]">
-          {tabs.map((item, idx) => (
-            <TabLink hash={item.hash} key={idx}>
-              {item.name}
-            </TabLink>
-          ))}
-        </Tabs>
-        <div className="relative overflow-hidden pb-12">
-          <div className="absolute top-0 w-full h-[100px] opacity-40 bg-[linear-gradient(180deg,_rgba(124,_58,_237,_0.06)_0%,_rgba(72,_58,_237,_0)_100%)]"></div>
-          <div className="relative container-custom-screen mt-12">
-            <div className="prose text-slate-100">
-              <p>
-                WunderGraph is an open source API developer platform with a focus on developer experience. At its core,
-                WunderGraph combines the API gateway pattern with the Backend for Frontend (BFF) patternto create the
-                perfect developer experience for working with APIs.
-              </p>
-              <p>
-                WunderGraph can combine all of your services, databases such as Fauna, file storage, identity providers
-                (IdPs), and third-party APIs into your own developer toolkit without experiencing vendor lock-in.
-              </p>
+      <Tabs className="mt-20 sticky top-[3.75rem] z-30 bg-slate-900 md:top-[4.3rem]">
+        {tabs.map((item, idx) => (
+          <TabLink hash={item.hash} key={idx}>
+            {item.name}
+          </TabLink>
+        ))}
+      </Tabs>
+      <div className="space-y-20">
+        <div>
+          <div className="relative overflow-hidden pb-12">
+            <div className="absolute top-0 w-full h-[100px] opacity-40 bg-[linear-gradient(180deg,_rgba(124,_58,_237,_0.06)_0%,_rgba(72,_58,_237,_0)_100%)]"></div>
+            <div className="relative container-custom-screen mt-12">
+              <div className="prose text-slate-100">
+                <p>
+                  WunderGraph is an open source API developer platform with a focus on developer experience. At its
+                  core, WunderGraph combines the API gateway pattern with the Backend for Frontend (BFF) patternto
+                  create the perfect developer experience for working with APIs.
+                </p>
+                <p>
+                  WunderGraph can combine all of your services, databases such as Fauna, file storage, identity
+                  providers (IdPs), and third-party APIs into your own developer toolkit without experiencing vendor
+                  lock-in.
+                </p>
+              </div>
+              <div className="mt-6 flex flex-wrap gap-3 items-center">
+                <h3 className="text-sm text-slate-400 font-medium">Classified in</h3>
+                <TagsGroup>
+                  <Tag>Dev Tools</Tag>
+                  <Tag>Open Source</Tag>
+                  <Tag>Framework</Tag>
+                </TagsGroup>
+              </div>
             </div>
-            <div className="mt-6 flex flex-wrap gap-3 items-center">
-              <h3 className="text-sm text-slate-400 font-medium">Classified in</h3>
-              <TagsGroup>
-                <Tag>Dev Tools</Tag>
-                <Tag>Open Source</Tag>
-                <Tag>Framework</Tag>
-              </TagsGroup>
+            <div className="max-w-screen-2xl mt-10 mx-auto sm:px-8">
+              <Gallery>
+                {product_images.map((item, idx) => (
+                  <GalleryImage key={idx} src={item} alt="" />
+                ))}
+              </Gallery>
             </div>
           </div>
-          <div className="max-w-screen-2xl mt-10 mx-auto sm:px-8">
-            <Gallery>
-              {product_images.map((item, idx) => (
-                <GalleryImage key={idx} src={item} alt="" />
+        </div>
+        <div className="container-custom-screen" id="comments">
+          <h3 className="text-slate-50 font-medium">Support and give a Feedback</h3>
+          <CommentForm className="mt-12">
+            <CommentFormWrapper>
+              <CommentUserAvatar src="/images/random-user.jpg" />
+              <CommentTextarea placeholder="Write your feedback" />
+            </CommentFormWrapper>
+            <div className="mt-3 flex justify-end">
+              <Button className="text-sm bg-slate-800 hover:bg-slate-700">Comment</Button>
+            </div>
+          </CommentForm>
+          <div className="mt-6">
+            <Comments>
+              {comments.map((item, idx) => (
+                <Comment key={idx}>
+                  <CommentUserAvatar src={item.user.avatar} />
+                  <div>
+                    <CommentUserName>{item.user.name}</CommentUserName>
+                    <CommentDate>{item.date}</CommentDate>
+                    <CommentContext className="mt-3">{item.context}</CommentContext>
+                    <CommentLike className="mt-2" count={1} />
+                  </div>
+                </Comment>
               ))}
-            </Gallery>
+            </Comments>
           </div>
         </div>
-      </div>
-      <div className="container-custom-screen" id="comments">
-        <h3 className="text-slate-50 font-medium">Support and give a Feedback</h3>
-        <CommentForm className="mt-12">
-          <CommentFormWrapper>
-            <CommentUserAvatar src="/images/random-user.jpg" />
-            <CommentTextarea placeholder="Write your feedback" />
-          </CommentFormWrapper>
-          <div className="mt-3 flex justify-end">
-            <Button className="text-sm bg-slate-800 hover:bg-slate-700">Comment</Button>
+        <div className="container-custom-screen" id="details">
+          <h3 className="text-slate-50 font-medium">About this launch</h3>
+          <p className="text-slate-300 mt-6">
+            WunderGraph was hunted by Alexander Isora 🦄 in Design Tools, Developer Tools. Made by Sidi jeddou. Featured
+            on March 24th, 2023.
+          </p>
+          <div className="mt-10">
+            <StatsWrapper>
+              {stats.map((item, idx) => (
+                <Stat key={idx} className="py-4">
+                  <StatCountItem>{item.count}</StatCountItem>
+                  <StatItem className="mt-2">
+                    {item.icon}
+                    {item.label}
+                  </StatItem>
+                </Stat>
+              ))}
+            </StatsWrapper>
           </div>
-        </CommentForm>
-        <div className="mt-6">
-          <Comments>
-            {comments.map((item, idx) => (
-              <Comment key={idx}>
-                <CommentUserAvatar src={item.user.avatar} />
-                <div>
-                  <CommentUserName>{item.user.name}</CommentUserName>
-                  <CommentDate>{item.date}</CommentDate>
-                  <CommentContext className="mt-3">{item.context}</CommentContext>
-                  <CommentLike className="mt-2" count={1} />
-                </div>
-              </Comment>
-            ))}
-          </Comments>
         </div>
-      </div>
-      <div className="container-custom-screen" id="details">
-        <h3 className="text-slate-50 font-medium">About this launch</h3>
-        <p className="text-slate-300 mt-6">
-          WunderGraph was hunted by Alexander Isora 🦄 in Design Tools, Developer Tools. Made by Sidi jeddou. Featured
-          on March 24th, 2023.
-        </p>
-        <div className="mt-10">
-          <StatsWrapper>
-            {stats.map((item, idx) => (
-              <Stat key={idx} className="py-4">
-                <StatCountItem>{item.count}</StatCountItem>
-                <StatItem className="mt-2">
-                  {item.icon}
-                  {item.label}
-                </StatItem>
-              </Stat>
+        <div className="container-custom-screen" id="launches">
+          <h3 className="text-slate-50 font-medium">Related launches</h3>
+          <ul className="mt-6 grid divide-y divide-slate-800/60 md:grid-cols-2 md:divide-y-0">
+            {mockproducts.map((item, idx) => (
+              <li key={idx} className="py-3">
+                <ProductCard href={item.slug}>
+                  <Logo src={item.logo} alt={item.title} imgClassName="w-14 h-14" />
+                  <div className="space-y-1">
+                    <Name>{item.name}</Name>
+                    <Title className="line-clamp-1 sm:line-clamp-2">{item.title}</Title>
+                    <Tags items={['Free', 'Developer Tools']} />
+                  </div>
+                </ProductCard>
+              </li>
             ))}
-          </StatsWrapper>
+          </ul>
         </div>
-      </div>
-      <div className="container-custom-screen" id="launches">
-        <h3 className="text-slate-50 font-medium">Related launches</h3>
-        <ul className="mt-6 grid divide-y divide-slate-800/60 md:grid-cols-2 md:divide-y-0">
-          {mockproducts.map((item, idx) => (
-            <li key={idx} className="py-3">
-              <ProductCard href={item.slug}>
-                <Logo src={item.logo} alt={item.title} imgClassName="w-14 h-14" />
-                <div className="space-y-1">
-                  <Name>{item.name}</Name>
-                  <Title className="line-clamp-1 sm:line-clamp-2">{item.title}</Title>
-                  <Tags items={['Free', 'Developer Tools']} />
-                </div>
-              </ProductCard>
-            </li>
-          ))}
-        </ul>
       </div>
     </section>
   )
