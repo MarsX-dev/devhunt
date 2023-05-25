f3ae5c4744ffbb49e57716c743c70cdf671f8e35
mac <mac@guests-MacBook-Pro.local>
2023-05-25 12:29:49 +0000

    Improve shiny color

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -12,7 +12,7 @@ interface Props extends HTMLAttributes<HTMLButtonElement> {
 export default ({ children, className = '', variant = "default", isLoad = false, ...props }: Props) => {
 
   const variants = {
-    shiny: "py-3 px-4 font-medium text-center text-white active:shadow-none rounded-lg shadow bg-slate-800 md:bg-[linear-gradient(179.23deg,_#1E293B_0.66%,_rgba(30,_41,_59,_0)_255.99%)] hover:bg-slate-700 duration-150",
+    shiny: "py-3 px-4 font-medium text-center text-white active:shadow-none rounded-lg shadow bg-slate-700 md:bg-[linear-gradient(179.23deg,_#1E293B_0.66%,_rgba(30,_41,_59,_0)_255.99%)] hover:bg-slate-800 duration-150",
     default: "py-3 px-4 rounded-lg duration-150 font-medium text-center text-sm text-white bg-indigo-500"
   }
 
