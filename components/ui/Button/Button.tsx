e7513faa0638a91a6472731f02c5dc2d33b51a69
mohamed-abdelahi-haibelty <96340736+mohamed-abdelahi-haibelty@users.noreply.github.com>
2023-05-25 22:38:41 +0100

    update button component by adding on click

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -3,12 +3,14 @@ import { ReactNode } from 'react'
 
 type Props = {
   children: ReactNode
-  className?: string
+  className?: string,
+  onClick?: () => void
 }
 
-export default ({ children, className = '', ...props }: Props) => (
+export default ({ children, className = '', onClick, ...props }: Props) => (
   <button
     {...props}
+    onClick={() => onClick?.()}
     className={mergeTW(
       `py-3 px-4 rounded-lg duration-150 font-medium text-center text-sm text-white bg-indigo-500 ${className}`
     )}
