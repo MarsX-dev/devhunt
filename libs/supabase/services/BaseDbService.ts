117981cea2e2e7ee6d3ab28b318a72be182e7951
Vitalik May <vmaykovsky@users.noreply.github.com>
2023-05-26 11:44:05 +0300

    Fetch supabase types, minor fixes

--- libs/supabase/services/BaseDbService.ts
+++ libs/supabase/services/BaseDbService.ts
@@ -4,9 +4,9 @@ import { type Database } from '../types'
 import { createBrowserClient } from '@/libs/supabase/browser'
 
 export default abstract class BaseDbService {
-  constructor(private _isServer: boolean) {}
+  constructor (private readonly _isServer: boolean) {}
 
-  get supabase(): SupabaseClient<Database> {
+  get supabase (): SupabaseClient<Database> {
     if (this._isServer) {
       return createServerClient()
     }
