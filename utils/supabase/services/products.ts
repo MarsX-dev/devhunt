beb0dbae35712b9743ede9d3498272da89bf4696
Vitalik May <vitaly.maykovsky@gmail.com>
2023-06-01 12:29:36 +0300

    Minor fixes

--- utils/supabase/services/products.ts
+++ utils/supabase/services/products.ts
@@ -1,32 +1,31 @@
-import { ExtendedProduct } from '@/utils/supabase/CustomTypes'
-import BaseDbService from '@/utils/supabase/services/BaseDbService'
-import { InsertProduct, Product, UpdateProduct } from '@/utils/supabase/types'
-import { omit } from '@/utils/helpers'
+import { type ExtendedProduct } from '@/utils/supabase/CustomTypes';
+import BaseDbService from '@/utils/supabase/services/BaseDbService';
+import { type InsertProduct, type Product, type UpdateProduct } from '@/utils/supabase/types';
+import { omit } from '@/utils/helpers';
 
 export default class ProductsService extends BaseDbService {
-
-  private readonly EXTENDED_PRODUCT_SELECT = '*, product_pricing_types(*), product_categories(name), profiles (full_name)'
+  private readonly EXTENDED_PRODUCT_SELECT = '*, product_pricing_types(*), product_categories(name), profiles (full_name)';
 
   getProducts(sortBy: string = 'votes_count', ascending: boolean = false) {
     // there is error in types? foreignTable is required for order options, while it's not
-    //@ts-ignore
+    // @ts-expect-error
     return this.supabase
       .from('products')
       .select(this.EXTENDED_PRODUCT_SELECT)
       .eq('deleted', false)
-      .order(sortBy, { ascending })
+      .order(sortBy, { ascending });
   }
 
   async getSimilarProducts(productId: number): Promise<Product[]> {
-    const { data, error } = await this.supabase.rpc('get_similar_products', { _product_id: productId })
-    if (error !== null) throw new Error(error.message)
-    return data
+    const { data, error } = await this.supabase.rpc('get_similar_products', { _product_id: productId });
+    if (error !== null) throw new Error(error.message);
+    return data;
   }
 
   async getTopProducts(sortBy: string, ascending: boolean): Promise<ExtendedProduct[]> {
-    const { data: products, error } = await this.getProducts(sortBy, ascending)
-    if (error) throw new Error(error.message)
-    return products as ExtendedProduct[]
+    const { data: products, error } = await this.getProducts(sortBy, ascending);
+    if (error) throw new Error(error.message);
+    return products as ExtendedProduct[];
   }
 
   async getMostDiscussedProducts(limit = 10): Promise<ExtendedProduct[]> {
@@ -35,10 +34,10 @@ export default class ProductsService extends BaseDbService {
       .select(this.EXTENDED_PRODUCT_SELECT)
       .eq('deleted', false)
       .order('comments_count', { ascending: false })
-      .limit(limit)
+      .limit(limit);
 
-    if (error !== null) throw new Error(error.message)
-    return data as ExtendedProduct[]
+    if (error !== null) throw new Error(error.message);
+    return data as ExtendedProduct[];
   }
 
   async getRelatedProducts(
@@ -48,24 +47,24 @@ export default class ProductsService extends BaseDbService {
     ascending: boolean
   ): Promise<ExtendedProduct[]> {
     const { data: products, error } = await this.getProducts(sortBy, ascending)
-      .neq('id', productId)
+      .neq('id', productId);
 
     if (error) {
-      console.error(error)
-      return []
+      console.error(error);
+      return [];
     }
 
     const filteredProducts = products
       .filter(item => item.product_categories?.some(category => categoryNames.includes(category.name)))
-      .slice(0, 8)
+      .slice(0, 8);
 
-    return filteredProducts as ExtendedProduct[]
+    return filteredProducts as ExtendedProduct[];
   }
 
   async getUserProductsById(userId: string, sortBy: string, ascending: boolean) {
     const { data } = await this.getProducts(sortBy, ascending)
-      .eq('owner_id', userId)
-    return data || null
+      .eq('owner_id', userId);
+    return data || null;
   }
 
   async getUserVoteById(userId: string, productId: number) {
@@ -74,68 +73,68 @@ export default class ProductsService extends BaseDbService {
       .select()
       .eq('user_id', userId)
       .eq('product_id', productId)
-      .single()
-    return data || null
+      .single();
+    return data || null;
   }
 
   async getRandomTools(limit: number): Promise<ExtendedProduct[] | null> {
     const { data } = await this.supabase.from('products')
       .select(this.EXTENDED_PRODUCT_SELECT)
-      .limit(limit)
+      .limit(limit);
 
-    return data || null
+    return data;
   }
 
-  getById(id: number): Promise<ExtendedProduct | null> {
-    return this._getOne('id', id)
+  async getById(id: number): Promise<ExtendedProduct | null> {
+    return await this._getOne('id', id);
   }
 
-  getBySlug(slug: string): Promise<ExtendedProduct | null> {
-    return this._getOne('slug', slug)
+  async getBySlug(slug: string): Promise<ExtendedProduct | null> {
+    return await this._getOne('slug', slug);
   }
 
   async voteUnvote(productId: number, userId: string): Promise<number> {
-    const { data, error } = await this.supabase.rpc('triggerProductVote', { _product_id: productId, _user_id: userId })
+    const { data, error } = await this.supabase.rpc('triggerProductVote', { _product_id: productId, _user_id: userId });
 
     if (error !== null) {
-      throw new Error(error.message)
+      throw new Error(error.message);
     }
 
-    const product = await this._getOne('id', productId, 'votes_count')
+    const product = await this._getOne('id', productId, 'votes_count');
 
-    return product?.votes_count || 0
+    return product?.votes_count || 0;
   }
 
   async update(id: number, updates: UpdateProduct): Promise<Product> {
-    const cleanUpdates = omit(updates, ['deleted_at', 'deleted'])
+    const cleanUpdates = omit(updates, ['deleted_at', 'deleted']);
 
-    const { data, error } = await this.supabase.from('products').update(cleanUpdates).eq('id', id).single()
+    const { data, error } = await this.supabase.from('products').update(cleanUpdates).eq('id', id).single();
 
-    if (error != null) throw new Error(error.message)
+    if (error != null) throw new Error(error.message);
 
-    return data as Product
+    return data as Product;
   }
 
   async delete(id: number): Promise<void> {
     const { error } = await this.supabase
       .from('products')
       .update({ deleted: true, deleted_at: new Date() })
-      .eq('id', id)
+      .eq('id', id);
 
-    if (error !== null) throw new Error(error.message)
+    if (error !== null) throw new Error(error.message);
   }
 
   async insert(product: InsertProduct): Promise<Product | null> {
-    const { data, error } = await this.supabase.from('products').insert(product).select().single()
-    if (error !== null) throw new Error(error.message)
-    return data
+    const { data, error } = await this.supabase.from('products').insert(product).select().single();
+    if (error !== null) throw new Error(error.message);
+    return data;
   }
 
   async search(searchTerm: string): Promise<Product[] | null> {
-    const { data, error } = await this.supabase.from('products').select('*').ilike('name', `%${searchTerm}%`).limit(5)
+    const { data, error } = await this.supabase.from('products').select('*').ilike('name', `%${searchTerm}%`).limit(5);
 
-    if (error !== null) throw new Error(error.message)
-    return data
+    if (error !== null) throw new Error(error.message);
+    return data;
   }
 
   private async _getOne(
@@ -148,17 +147,17 @@ export default class ProductsService extends BaseDbService {
       .select(select)
       .eq('deleted', false)
       .eq(column, value)
-      .limit(1)
+      .limit(1);
 
     if (error !== null) {
-      throw new Error(error.message)
+      throw new Error(error.message);
     }
 
-    const product = products[0]
+    const product = products[0];
     if (!product) {
-      return null
+      return null;
     }
 
-    return product
+    return product;
   }
 }
