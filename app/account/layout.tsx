79798bca9f33e193c9c797466d7d4034a904a179
mac <mac@guests-MacBook-Pro.local>
2023-06-03 15:37:30 +0000

    Remove lock files

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import Protectedroute from '../../components/ProtectedRoute';
+import ProtectedRoute from '@/components/ProtectedRoute';
 import { ReactNode } from 'react';
 
 export default ({ children }: { children: ReactNode }) => (
-  <Protectedroute>
+  <ProtectedRoute>
     <div className="mt-10 mb-32">{children}</div>
-  </Protectedroute>
+  </ProtectedRoute>
 );
