99d66356f6ec52e0251b5541741b7d792483981d
mac <mac@guests-MacBook-Pro.local>
2023-06-03 15:42:22 +0000

    Make ProtectedRoute client side

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import ProtectedRoute from '@/components/ProtectedRoute';
 import { ReactNode } from 'react';
 
--- components/Protectedroute.tsx
+++ components/Protectedroute.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { useSupabase } from '@/components/supabase/provider';
 import { ReactNode } from 'react';
 import LoginPage from './ui/LoginPage';
