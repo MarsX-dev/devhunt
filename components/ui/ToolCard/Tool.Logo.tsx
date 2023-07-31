47766175327f9ac31748ca12306ca0acce260be7
mac <mac@guests-MacBook-Pro.local>
2023-07-31 13:09:40 +0000

    Remove image components

--- components/ui/ToolCard/Tool.Logo.tsx
+++ components/ui/ToolCard/Tool.Logo.tsx
@@ -1,8 +1,7 @@
 import mergeTW from '@/utils/mergeTW';
-import Image from 'next/image';
 
 export default ({ src, className, imgClassName, alt }: { src: string; className?: string; imgClassName?: string; alt?: string }) => (
   <div className={mergeTW(`flex-none  ${className}`)}>
-    <Image src={src} alt={alt as string} className={mergeTW(`rounded-full object-fill  ${imgClassName}`)} width={65} height={65} priority />
+    <img src={src} alt={alt as string} className={mergeTW(`rounded-full w-16 h-16  ${imgClassName}`)} />
   </div>
 );
