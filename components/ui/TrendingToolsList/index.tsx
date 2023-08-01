7abb4318507a3060814983b2e7b8b8e739e1e3d9
mac <mac@guests-MacBook-Pro.local>
2023-08-01 23:28:24 +0000

    Stop trending tools to check the perf

--- components/ui/TrendingToolsList/index.tsx
+++ components/ui/TrendingToolsList/index.tsx
@@ -1,40 +1,40 @@
 'use client';
 
-import ToolName from '@/components/ui/ToolCard/Tool.Name';
-import Tags from '@/components/ui/ToolCard/Tool.Tags';
-import Title from '@/components/ui/ToolCard/Tool.Title';
-import ToolCard from '@/components/ui/ToolCard/ToolCard';
-import { createBrowserClient } from '@/utils/supabase/browser';
-import ToolVotes from '@/components/ui/ToolCard/Tool.Votes';
-import ToolFooter from '@/components/ui/ToolCard/Tool.Footer';
-import ToolViews from '@/components/ui/ToolCard/Tool.views';
-import { useEffect, useState } from 'react';
+// import ToolName from '@/components/ui/ToolCard/Tool.Name';
+// import Tags from '@/components/ui/ToolCard/Tool.Tags';
+// import Title from '@/components/ui/ToolCard/Tool.Title';
+// import ToolCard from '@/components/ui/ToolCard/ToolCard';
+// import { createBrowserClient } from '@/utils/supabase/browser';
+// import ToolVotes from '@/components/ui/ToolCard/Tool.Votes';
+// import ToolFooter from '@/components/ui/ToolCard/Tool.Footer';
+// import ToolViews from '@/components/ui/ToolCard/Tool.views';
+import { useState } from 'react';
 import { ProductType } from '@/type';
-import ProductsService from '@/utils/supabase/services/products';
-import ToolLogo from '@/components/ui/ToolCard/Tool.Logo';
+// import ProductsService from '@/utils/supabase/services/products';
+// import ToolLogo from '@/components/ui/ToolCard/Tool.Logo';
 
-const getTrendingTools = async () => {
-  const today = new Date();
-  const productService = new ProductsService(createBrowserClient());
-  const week = await productService.getWeekNumber(today, 2);
-  return await productService.getPrevLaunchWeeks(today.getFullYear(), 2, week, 5);
-};
+// const getTrendingTools = async () => {
+//   const today = new Date();
+//   const productService = new ProductsService(createBrowserClient());
+//   const week = await productService.getWeekNumber(today, 2);
+//   return await productService.getPrevLaunchWeeks(today.getFullYear(), 2, week, 5);
+// };
 
 export default () => {
   const [trendingTools, setTrendingTools] = useState<ProductType[]>([]);
 
-  useEffect(() => {
-    getTrendingTools().then(tools => {
-      console.log(tools);
+  // useEffect(() => {
+  //   getTrendingTools().then(tools => {
+  //     console.log(tools);
 
-      const allTools = tools?.map(tool => tool);
-      setTrendingTools(allTools as any);
-    });
-  }, []);
+  //     const allTools = tools?.map(tool => tool);
+  //     setTrendingTools(allTools as any);
+  //   });
+  // }, []);
 
   return (
     <ul className="mt-3 divide-y divide-slate-800/60">
-      {trendingTools?.map((group, idx) => (
+      {/* {trendingTools?.map(group => (
         <div>
           {group.products.map((tool, idx) => (
             <li key={idx} className="py-3">
@@ -53,7 +53,7 @@ export default () => {
             </li>
           ))}
         </div>
-      ))}
+      ))} */}
     </ul>
   );
 };
