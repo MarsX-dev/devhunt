ca4e083f8c7497647f773b0e666c13c77f35326c
igorboky <igorboky@gmail.com>
2023-08-06 01:26:52 +0300

    Added revalidation for tool and home page

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,8 @@ const { title, description, ogImage } = {
   ogImage: 'https://devhunt.org/devhuntog.png?v=2',
 };
 
+export const revalidate = 60;
+
 export const metadata = {
   title,
   description,
