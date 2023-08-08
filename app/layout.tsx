309b19a4487b00ef966529153e837bc1f2939bd1
John Rush <john@theworldx.com>
2023-08-09 02:10:19 +0300

    hot fix for clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,13 +59,13 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           `,
               }}
             />
-            <Script dangerouslySetInnerHTML={{__html:`<script type="text/javascript">
+            <Script strategy="afterInteractive" dangerouslySetInnerHTML={{__html:`
     (function(c,l,a,r,i,t,y){
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
     })(window, document, "clarity", "script", "ic87ytbm3p");
-</script>`}} />
+`}} />
           </>
         )}
         <meta name="twitter:card" content="summary_large_image" />
