e3ebc298217029c14a91b76310b2377790335a51
sidiDev <sididev3@gmail.com>
2023-09-19 12:02:55 +0000

    Add ref to the link

--- components/ui/ToolCard/Tool.Name.tsx
+++ components/ui/ToolCard/Tool.Name.tsx
@@ -5,7 +5,13 @@ import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/solid';
 export default ({ className, children, href }: { className?: string; href: string; children?: ReactNode }) => (
   <h3 className={mergeTW(`text-slate-100 font-medium flex gap-x-3 items-center ${className}`)}>
     {children}
-    <a id="tool-title" href={href} onClick={() => window.open(href)} target="_blank" className="hidden group-hover/card:block">
+    <a
+      id="tool-title"
+      href={`${href}?ref=devhunt`}
+      onClick={() => window.open(href)}
+      target="_blank"
+      className="hidden group-hover/card:block"
+    >
       <ArrowTopRightOnSquareIcon className="w-4 h-4 pointer-events-none" />
     </a>
   </h3>
