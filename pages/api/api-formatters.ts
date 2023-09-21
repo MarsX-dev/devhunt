2cdcc09942a318954a8c154baa127b8ded9179c0
Vitalik May <7137872+vitalikmay@users.noreply.github.com>
2023-09-21 09:50:19 +0300

    Past week tools API

--- pages/api/api-formatters.ts
+++ pages/api/api-formatters.ts
@@ -1,15 +1,15 @@
-import {ExtendedProduct} from "@/utils/supabase/CustomTypes";
+import { ExtendedProduct } from '@/utils/supabase/CustomTypes';
 
-export function simpleToolApiDtoFormatter(t: ExtendedProduct){
-    return {
-        id: t.id,
-        name: t.name,
-        description: t.description,
-        logo_url: t.logo_url,
-        data_added: new Date(t.created_at).toISOString().split('T')[0],
-        launch_date: t.launch_date,
-        votes_count: t.votes_count || '-',
-        devhunt_link: `https://devhunt.org/tool/${t.slug}`,
-        link: t.demo_url
-    };
+export function simpleToolApiDtoFormatter(t: ExtendedProduct) {
+  return {
+    id: t.id,
+    name: t.name,
+    description: t.description,
+    logo_url: t.logo_url,
+    data_added: new Date(t.created_at).toISOString().split('T')[0],
+    launch_date: t.launch_date,
+    votes_count: t.votes_count || '-',
+    devhunt_link: `https://devhunt.org/tool/${t.slug}`,
+    link: t.demo_url,
+  };
 }
