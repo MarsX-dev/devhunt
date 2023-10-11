208af012e81a94a6d4dd2d3873f9c086f65d55e4
John Rush <john@theworldx.com>
2023-10-11 23:04:47 +0300

    improved blog style

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -19,31 +19,31 @@ export default async function Blog({ searchParams: { page } }: { searchParams: {
   const lastPage = Math.ceil(total / 10);
 
   return (
-    <section className="max-w-4xl mt-5 lg:mt-10 mx-auto px-4 md:px-8 dark:text-white">
+    <section className="max-w-3xl mt-5 lg:mt-10 mx-auto px-4 md:px-8 dark:text-white tracking-normal">
       <h1 className="text-4xl mb-4 font-black">DevHunt's Blog</h1>
       <ul>
         {posts.map((i: any) => (
-          <li key={i.id} className="border-b border-gray-200 dark:border-slate-700 py-6">
-            <div className="flex flex-wrap gap-2 items-center w-full text-sm dark:text-slate-400">
+          <li key={i.id} className="border-b border-gray-200 dark:border-slate-800 py-8">
+            <div className="flex flex-wrap gap-2 items-center w-full text-sm dark:text-slate-500">
               <span>
                 Published{' '}
                 {new Date(i.publishedAt || i.createdAt).toLocaleDateString('en-US', { day: 'numeric', month: 'short', year: 'numeric' })}
               </span>
               {i.readingTime ? <span>{` ⦁ ${i.readingTime}`} min read</span> : null}
             </div>
-            <Link href={`/blog/${i.slug}`} className="block mt-1 mb-3 text-2xl font-bold">
+            <Link href={`/blog/${i.slug}`} className="block mt-2 mb-3 font-medium">
               {i.headline}
             </Link>
-            <div className="mb-3 w-full dark:text-slate-400">{i.metaDescription}</div>
+            <div className="text-slate-400 text-sm sm:text-base line-clamp-2 line mb-4 block">{i.metaDescription}</div>
             <div className="flex flex-wrap justify-between gap-3">
               <div className="flex flex-wrap gap-2">
                 {(i.tags || []).splice(0, 3).map((t: any, ix: number) => (
-                  <a key={ix} href={`/blog/tag/${t.slug}`} className="bg-slate-700 px-3 rounded">
+                  <a key={ix} href={`/blog/tag/${t.slug}`} className="bg-slate-800 px-2 py-1 rounded text-xs text-slate-400">
                     {t.title}
                   </a>
                 ))}
               </div>
-              <Link href={`/blog/${i.slug}`} className="self-end font-bold">
+              <Link href={`/blog/${i.slug}`} className="flex items-center text-sm text-orange-500 hover:text-orange-400 font-medium">
                 Read More →
               </Link>
             </div>
@@ -52,7 +52,7 @@ export default async function Blog({ searchParams: { page } }: { searchParams: {
       </ul>
       {lastPage > 1
         ? (
-        <div className="flex mt-6 items-center justify-center">
+        <div className="flex mt-12 items-center justify-center text-sm text-slate-300">
           <a
             className={`border rounded-md px-2 py-1 w-[90px] text-center ${pageNumber ? '' : 'pointer-events-none opacity-30'}`}
             href={pageNumber ? `/blog?page=${pageNumber}` : '#'}
