e9419de1885cba4613df63a1e635640a9840a5cf
Vitalik May <7137872+vitalikmay@users.noreply.github.com>
2023-10-12 13:42:57 +0300

    SeoBot Blog Sitemap

---
+++ app/blog/sitemap.xml/route.tsx
@@ -0,0 +1,44 @@
+const BASE_URL = 'https://devhunt.org';
+
+async function getSitemap() {
+  const key = process.env.SEOBOT_API_KEY;
+  if (!key) throw Error('SEOBOT_API_KEY enviroment variable must be set');
+
+  try {
+    const res = await fetch(`https://app.seobotai.com/api/sitemap?key=${key}`);
+    const result = await res.json();
+    return result?.data;
+  } catch {
+    return { articles: [], categories: [], tags: [] };
+  }
+}
+
+function toSitemapRecord(loc: string, updatedAt: string) {
+  return `<url><loc>${new URL(loc, BASE_URL).toString()}</loc><lastmod>${updatedAt}</lastmod></url>`;
+}
+
+async function generateSiteMap() {
+  const blogSitemap = await getSitemap();
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://devhunt.org/blog</loc>
+    </url>
+     ${blogSitemap.articles.map(i => toSitemapRecord(`/blog/${i.slug}`, i.lastmod))}
+     ${blogSitemap.categories.map(i => toSitemapRecord(`/blog/category/${i.slug}`, i.lastmod))}
+     ${blogSitemap.tags.map(i => toSitemapRecord(`/blog/tag/${i.slug}`, i.lastmod))}
+   </urlset>
+ `;
+}
+
+export async function GET() {
+  const body = await generateSiteMap();
+
+  return new Response(body, {
+    status: 200,
+    headers: {
+      'Cache-control': 'public, s-maxage=86400, stale-while-revalidate',
+      'content-type': 'application/xml',
+    },
+  });
+}
