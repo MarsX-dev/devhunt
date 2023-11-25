f2f61f975f5db25972840dd4f5c343da3e45f60b
sidiDev <sididev3@gmail.com>
2023-11-25 12:52:04 +0000

    Remove the banner

--- components/ui/Banner/Banner.tsx
+++ components/ui/Banner/Banner.tsx
@@ -3,24 +3,24 @@ import LinkItem from '../Link/LinkItem';
 
 export default () => {
   return (
-    <div className="bg-orange-600">
-      <div className="custom-screen py-3 items-center gap-x-4 justify-between text-white font-medium sm:flex md:px-8">
-        <div className="flex items-center">
-          <img src="https://user-uploads-unicorn.s3.amazonaws.com/product_hunt_kitty.png" alt="PH kitty" className="w-24" />
-          <p>Support our project: Float UI on Product Hunt now</p>
-        </div>
-        <LinkItem
-          href="https://www.producthunt.com/posts/float-ui-v2"
-          target="_blank"
-          className="flex-none inline-block w-full mt-3 py-2 text-center text-orange-600 font-medium bg-white duration-150 hover:bg-gray-100 active:bg-gray-200 sm:w-auto sm:mt-0 btnshake"
-        >
-          <div className="flex items-center justify-center gap-x-1 text-sm font-semibold">
-            Check it out
-            <IconArrowLongRight className="w-4 h-4" />
-          </div>
-        </LinkItem>
-      </div>
-    </div>
-    // <></>
+    // <div className="bg-orange-600">
+    //   <div className="custom-screen py-3 items-center gap-x-4 justify-between text-white font-medium sm:flex md:px-8">
+    //     <div className="flex items-center">
+    //       <img src="https://user-uploads-unicorn.s3.amazonaws.com/product_hunt_kitty.png" alt="PH kitty" className="w-24" />
+    //       <p>Support our project: Float UI on Product Hunt now</p>
+    //     </div>
+    //     <LinkItem
+    //       href="https://www.producthunt.com/posts/float-ui-v2"
+    //       target="_blank"
+    //       className="flex-none inline-block w-full mt-3 py-2 text-center text-orange-600 font-medium bg-white duration-150 hover:bg-gray-100 active:bg-gray-200 sm:w-auto sm:mt-0 btnshake"
+    //     >
+    //       <div className="flex items-center justify-center gap-x-1 text-sm font-semibold">
+    //         Check it out
+    //         <IconArrowLongRight className="w-4 h-4" />
+    //       </div>
+    //     </LinkItem>
+    //   </div>
+    // </div>
+    <></>
   );
 };
