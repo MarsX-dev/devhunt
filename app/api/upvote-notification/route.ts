1f311fed2cec21b78ab971e16f7297166e602c1a
sidiDev <sididev3@gmail.com>
2023-12-18 01:22:40 +0000

    Add securing cron code

--- app/api/upvote-notification/route.ts
+++ app/api/upvote-notification/route.ts
@@ -74,6 +74,13 @@ async function sendNotification(email: string, slug: string, product_name: strin
 }
 
 export async function GET(request: NextRequest) {
+  const authHeader = request.headers.get('authorization');
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', {
+      status: 401,
+    });
+  }
+
   const getToken = (await getAuthToken()).data.Token;
 
   const productsService = new ProductsService(createServerClient());
