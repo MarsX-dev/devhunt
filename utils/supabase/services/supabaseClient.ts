8c1d9e80af8aae7efb0c809a18991d91c27d3808
sidiDev <sididev3@gmail.com>
2023-12-18 01:14:13 +0000

    Use createClientComponentClient instead of createClient

--- utils/supabase/services/supabaseClient.ts
+++ utils/supabase/services/supabaseClient.ts
@@ -1,8 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL as string, process.env.SUPABASE_SERVICE_ROLE_KEY as string, {
-  auth: {
-    autoRefreshToken: false,
-    persistSession: false,
-  },
+export const supabase = createClientComponentClient({
+  supabaseKey: process.env.SUPABASE_SERVICE_ROLE_KEY as string,
+  supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL,
 });
