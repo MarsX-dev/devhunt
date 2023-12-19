fcfc9d2bfb147f3d43fa08a03274b5e3b949267d
sidiDev <sididev3@gmail.com>
2023-12-19 01:06:27 +0000

    Turn ToolCardEffect ssr to false

--- components/ui/ToolCardEffect/ToolCardEffect.tsx
+++ components/ui/ToolCardEffect/ToolCardEffect.tsx
@@ -10,10 +10,10 @@ import ToolFooter from '@/components/ui/ToolCard/Tool.Footer';
 import ToolViews from '@/components/ui/ToolCard/Tool.views';
 import { type ProductType } from '@/type';
 import { useInView } from 'framer-motion';
-import { useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { createBrowserClient } from '@/utils/supabase/browser';
 import ProductsService from '@/utils/supabase/services/products';
-import { LazyMotion, domAnimation } from 'framer-motion';
+import Link from 'next/link';
 
 export default ({ tool }: { tool: ProductType }) => {
   const cardRef = useRef(null);
@@ -24,27 +24,23 @@ export default ({ tool }: { tool: ProductType }) => {
   }
 
   return (
-    <LazyMotion features={domAnimation}>
-      <li ref={cardRef} className="py-3">
-        <ToolCard tool={tool} href={'/tool/' + tool.slug}>
-          <div className="w-full flex items-center gap-x-4">
-            <Logo src={tool.logo_url || ''} alt={tool.name} />
-            <div className="w-full space-y-1">
-              <Name href={tool.demo_url as string}>{tool.name}</Name>
-              <Title className="line-clamp-2">{tool.slogan}</Title>
-              <ToolFooter>
-                <Tags items={[tool.product_pricing_types?.title ?? 'Free', ...(tool.product_categories || []).map(c => c.name)]} />
-                <ToolViews count={tool.views_count} />
-              </ToolFooter>
-            </div>
+    <li ref={cardRef} className="py-3">
+      <ToolCard tool={tool} href={'/tool/' + tool.slug}>
+        <div className="w-full flex items-center gap-x-4">
+          <Logo src={tool.logo_url || ''} alt={tool.name} />
+          <div className="w-full space-y-1">
+            <Name href={tool.demo_url as string}>{tool.name}</Name>
+            <Title className="line-clamp-2">{tool.slogan}</Title>
+            <ToolFooter>
+              <Tags items={[tool.product_pricing_types?.title ?? 'Free', ...(tool.product_categories || []).map(c => c.name)]} />
+              <ToolViews count={tool.views_count} />
+            </ToolFooter>
           </div>
-          <div
-            className={`flex-1 self-center flex justify-end duration-1000 delay-150 ${isInView ? 'opacity-100 mt-0' : 'opacity-0 mt-8'}`}
-          >
-            <Votes count={tool.votes_count} productId={tool?.id} launchDate={tool.launch_date} launchEnd={tool.launch_end} />
-          </div>
-        </ToolCard>
-      </li>
-    </LazyMotion>
+        </div>
+        <div className={`flex-1 self-center flex justify-end duration-1000 delay-150 ${isInView ? 'opacity-100 mt-0' : 'opacity-0 mt-8'}`}>
+          <Votes count={tool.votes_count} productId={tool?.id} launchDate={tool.launch_date} launchEnd={tool.launch_end} />
+        </div>
+      </ToolCard>
+    </li>
   );
 };
