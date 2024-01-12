cf09c1e397aabceaad35f845c05af8abecbdbe7e
sidiDev <sididev3@gmail.com>
2024-01-12 21:11:00 +0000

    Fix the card elements

--- components/ui/ToolCard/Tool.Tags.tsx
+++ components/ui/ToolCard/Tool.Tags.tsx
@@ -1,9 +1,10 @@
 export default ({ items }: { items: any[] }) => (
-  <div className="flex items-center gap-x-3 text-sm text-slate-400 overflow-hidden">
+  <div className="flex flex-wrap items-center gap-x-3 text-sm text-slate-400">
     {items.slice(0, 3).map((item, idx) => (
       <>
+        {console.log(item)}
         <span className="flex-none">{item}</span>
-        {idx !== items.length - 1 ? <span className="block flex-none w-1 h-1 bg-slate-500 rounded-full"></span> : ''}
+        <span className="block flex-none w-1 h-1 bg-slate-500 rounded-full"></span>
       </>
     ))}
   </div>
--- components/ui/ToolCard/Tool.views.tsx
+++ components/ui/ToolCard/Tool.views.tsx
@@ -2,7 +2,6 @@ import mergeTW from '@/utils/mergeTW';
 
 export default ({ className, count }: { className?: string; count: number }) => (
   <div className={mergeTW(`flex gap-x-3 items-center text-sm text-slate-400 ${className}`)}>
-    <span className="block flex-none w-1 h-1 bg-slate-500 rounded-full"></span>
     <div className="flex items-center gap-x-1">
       {count}
       <span>Impressions</span>
