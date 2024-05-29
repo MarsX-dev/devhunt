395681e5c2d680c3335e776a472d09a532cda6e7
Sahil Atahar <sahilatahar@gmail.com>
2024-05-29 07:35:29 +0530

    fix: Edit profile page form override issue

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -7,5 +7,6 @@ import LoginPage from '../../components/ui/LoginPage';
 export default ({ children }: { children: ReactNode }) => {
   const { session } = useSupabase();
   const user = session?.user;
-  return user ? <div className="mt-10 mb-32">{children}</div> : <LoginPage />;
+
+  return user ? <div className="mt-10 min-h-[900px]">{children}</div> : <LoginPage />;
 };
