4212cbe297d4c65e3b33466b111fc930f45ac26a
John Rush <john@theworldx.com>
2024-10-23 16:28:28 +0300

    added new sponsor

--- components/ui/CountdownPanel/index.tsx
+++ components/ui/CountdownPanel/index.tsx
@@ -3,6 +3,46 @@
 import moment from 'moment';
 import { useEffect, useState } from 'react';
 
+const sponsorsData = [
+  {
+    type: 'sponsor',
+    link: 'https://dub.sh/marblism-devhunt',
+    title: 'Marblism.com',
+    description: 'NextJS apps with AI in minutes 😱',
+    features: ['Generate code', 'Host apps', 'Edit the code'],
+    callToAction: 'Try Now',
+  },
+  {
+    type: 'new',
+    link: 'https://dub.sh/listingbott',
+    title: 'ListingBott.com',
+    description: 'Automated Backlinks and Directory listings',
+    features: ['Good quality domains', 'Boost Domain Rating', 'Save days of work'],
+    callToAction: 'Try Now',
+  },
+];
+
+const SponsorCard = ({ sponsor }) => {
+  const { type, link, title, description, features, callToAction } = sponsor;
+
+  return (
+    <div className="mt-3 w-80 text-left sm:block border border-slate-700 bg-slate-900 rounded-md p-4 ">
+      <span className="text-xs mb-1 block text-slate-600">{type}</span>
+      <a href={link} target="_blank" rel="nofollow" className="text-slate-300 transition-opacity hover:text-slate-200">
+        <div className="text text-white mb-1 font-bold">{title}</div>
+
+        <div className="text-sm text-slate-300 justify-start mb-2 ">{description}</div>
+        {features.map((feature, index) => (
+          <div key={index}>
+            → <span className='text-sm'>{feature}</span>
+          </div>
+        ))}
+        <span className="mt-4 mb-3 block w-52 w-full bg-orange-900 text-white mx-0 p-1 text-sm rounded text-center">{callToAction}</span>
+      </a>
+    </div>
+  );
+};
+
 function RenderDatePart({ number, letter }: { number: number; letter: string }) {
   return (
     <div className="rounded-md p-[1px] overflow-hidden bg-gradient-to-b from-[#514b6130] to-[#514b6100]">
@@ -89,7 +129,7 @@ export default () => {
   return (
     <div className="flex border rounded-xl border-slate-800 bg-slate-800 p-5 flex-col gap-1 md:gap-2 items-center justify-end">
       <div className="px-2 flex flex-col items-center text-center gap-3">
-        <h1 className='text-slate-200 text-xl font-bold'>Find Best Dev Tools Voted by Developers!</h1>
+        <h1 className="text-slate-200 text-xl font-bold">Find Best Dev Tools Voted by Developers!</h1>
         <p>
           <span className="text-2xl font-bold mb-4 text-orange-500">Vote Closing In: </span>
         </p>
@@ -99,26 +139,20 @@ export default () => {
         <div className="max-w-lg mt-2 text-slate-400">
           100k+ developers found Dev Tools here.
           <div className="inline mt-2 sm:block">
-            See how  {' '}
-            <a className="underline transition-opacity hover:text-scale-1200" href="https://twitter.com/johnrushx/status/1661534492949872641">
-            it started
+            See how{' '}
+            <a
+              className="underline transition-opacity hover:text-scale-1200"
+              href="https://twitter.com/johnrushx/status/1661534492949872641"
+            >
+              it started
             </a>
             .
           </div>
-
-          <div className="mt-3 w-80 sm:block border border-orange-500 bg-slate-900 rounded-md p-3 pt-2 border-dasheds">
-            <span className='text-xs mb-2 block text-slate-600'>sponsor</span>  
-            <a target="_blank" rel='nofollow' className=" text-slate-300 transition-opacity hover:text-scale-1200" href="https://dub.sh/marblism-devhunt">
-            <span className='text-lg text-white font-bold'>Marblism.com</span> <br/> 
-            NextJS apps with AI in minutes 😱
-<br/>
-- Generate<br/>
-- Host<br/>
-- Edit the code
-           <span className='mt-4 mb-3 block w-52 mx-auto bg-orange-500 text-white p-1 text-sm rounded'>Try Now</span>
-            </a>
-            
-          </div> 
+          <div className="block sm:flex gap-2">
+            {sponsorsData.map((sponsor, index) => (
+              <SponsorCard key={index} sponsor={sponsor} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
