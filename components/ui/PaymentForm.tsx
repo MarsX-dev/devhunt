a89448b809fc52170f97fceff5177373325b9dbe
sidiDev <sididev3@gmail.com>
2025-05-29 08:47:00 +0000

    fix: optimize script loading by moving it outside modal and setting beforeInteractive strategy

--- components/ui/PaymentForm.tsx
+++ components/ui/PaymentForm.tsx
@@ -2,15 +2,17 @@ import Script from 'next/script';
 import Modal from './Modal';
 
 export default ({ isActive, toolName, email }: { isActive: boolean; toolName: string; email?: string }) => (
-  <Modal variant="custom" isActive={isActive} className="bg-white">
-    <iframe
-      loading="lazy"
-      id="rapidforms-iframe"
-      src={`https://app.rapidforms.co/embed/9365a8?Tool%20name=${toolName}&Email=${email}`}
-      width="100%"
-      height="550px"
-      frameBorder="0"
-    ></iframe>
-    <Script src="https://app.rapidforms.co/embed/index.js" />
-  </Modal>
+  <>
+    <Modal variant="custom" isActive={isActive} className="bg-white">
+      <iframe
+        loading="lazy"
+        id="rapidforms-iframe"
+        src={`https://app.rapidforms.co/embed/9365a8?Tool%20name=${toolName}&Email=${email}`}
+        width="100%"
+        height="550px"
+        frameBorder="0"
+      ></iframe>
+    </Modal>
+    <Script strategy="beforeInteractive" src="https://app.rapidforms.co/embed/index.js" />
+  </>
 );
